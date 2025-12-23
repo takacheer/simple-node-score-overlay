bb42ac69f42f8ba1fcae56a957de9068df45f7bc
takacheer <takahashi@klis.tsukuba.ac.jp>
2025-12-23 18:11:08 +0900

    stability and animation update

--- server.js
+++ server.js
@@ -2,25 +2,40 @@ const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const DATA_FILE = path.join(__dirname, 'data.json');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-let currentData = {
-    nameLeft: "TEAM A", scoreLeft: 0,
-    nameRight: "TEAM B", scoreRight: 0,
-    logoUrl: ""
-};
+function loadData() {
+    try {
+        const data = fs.readFileSync(DATA_FILE, 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error("Error reading data file:", err);
+        return { isVisible: false, nameLeft: "TEAM A", scoreLeft: 0, nameRight: "TEAM B", scoreRight: 0, logoUrl: "" };
+    }
+}
+
+function saveData(data) {
+    try {
+        fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+    } catch (err) {
+        console.error("Error writing data file:", err);
+    }
+}
 
 io.on('connection', (socket) => {
-    socket.emit('updateOverlay', currentData);
+    socket.emit('updateOverlay', loadData());
 
-    socket.on('sendUpdate', (data) => {
-        currentData = data;
-        io.emit('updateOverlay', data);
+    socket.on('sendUpdate', (newData) => {
+        saveData(newData); 
+        io.emit('updateOverlay', newData); 
     });
 });
 
